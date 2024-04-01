434a3ab71249d041e04e179f131c1c71fb16815c
bynelson <nelson.alvarezcedeno@gmail.com>
2024-04-01 09:25:04 -0600

    Added format function with yapf

--- pyramid/dagger/src/main.py
+++ pyramid/dagger/src/main.py
@@ -1,9 +1,9 @@
-"""A module for testing and building complex Python applications."""
+"""A module for testing and building Python applications."""
 
 import dagger
 
 from dagger import Doc, dag, field, function, object_type
-from typing import Self, Annotated
+from typing import Self, Annotated, Optional
 
 @object_type
 class Pyramid:
@@ -17,8 +17,50 @@ class Pyramid:
             version: Annotated[str, Doc("Python container version to use")] = "3.11-bullseye",
     ) -> Self:
         """Initialize container with a given python version."""
-
         self.ctr = self.ctr.from_("python:" + version)
+        return self
+
+    @function
+    def with_directory(
+            self,
+            directory: Annotated[dagger.Directory, Doc("Directory to run the formatter in")], 
+    ) -> Self:
+        """Sets the Pyramid working directory."""
+        self.ctr = self.ctr.with_mounted_directory("/app", directory).with_workdir("/app")
+        return self
+
+    @function
+    def with_pytest(self) -> Self:
+        """Install pytest on the Pyramid container."""
+        self.ctr = self.ctr.with_exec(["pip3", "install", "-U", "pytest"])
+        return self
+
+    @function
+    def with_yapf(self) -> Self:
+        """Install yapf on the Pyramid container."""
+        self.ctr = self.ctr.with_exec(["pip3", "install", "-U", "yapf"])
+        return self
+
+    @function
+    async def format(
+            self,
+            apply:   Annotated[bool, Doc("Whetever to run as test or format the code in place")] = False,
+            verbose: Annotated[bool, Doc("Whetever to get verbose output")] = False,
+    ) -> Self:
+        """Use yapf to format or test code."""
+        args = ["yapf", "--recursive", "--parallel"]
+
+        if apply:
+            args.append("--in-place")
+        else:
+            args.append("--diff")
+
+        if verbose:
+            args.append("--verbose")
+
+        args.append(".")
+
+        self.ctr = self.ctr.with_exec(args)
         return self
 
     @function
@@ -33,3 +75,9 @@ class Pyramid:
             self.ctr.with_exec(["python3", "--version"])
             .stdout()
         )
+
+    @function
+    async def get_directory(self) -> dagger.Directory:
+        """Get the working directory."""
+        directory = await self.ctr.workdir()
+        return self.ctr.directory(directory)
